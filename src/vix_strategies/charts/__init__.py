48d5a187940ee560ad79c93fc108d47099f1ba6c
archieve_0407 <101052964+srdayend@users.noreply.github.com>
2026-09-07 12:22:09 +0900

    feat: add consolidated FIXERS chart helpers

---
+++ src/vix_strategies/charts/__init__.py
@@ -0,0 +1,11 @@
+"""FIXERS-styled chart helpers."""
+
+from .fixers_style import (
+    FIXERS_COLORS,
+    SERIES_PALETTE,
+    apply_style,
+    make_line_chart,
+    make_bar_chart,
+)
+
+__all__ = ["FIXERS_COLORS", "SERIES_PALETTE", "apply_style", "make_line_chart", "make_bar_chart"]
